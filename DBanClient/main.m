b17c0a23723f486bd6ee0d071089d7e0194a2c14
ibokan <ibokan@bokanwensi3-24.local>
2013-04-15 18:32:37 -0700

    Initial Commit

---
+++ DBanClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DBanClient
+//
+//  Created by ibokan on 13-4-15.
+//  Copyright (c) 2013年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "zzdAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([zzdAppDelegate class]));
+    }
+}
---
+++ DBanClient/zzdAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  zzdAppDelegate.h
+//  DBanClient
+//
+//  Created by ibokan on 13-4-15.
+//  Copyright (c) 2013年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface zzdAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
